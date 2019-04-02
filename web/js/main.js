d26d7150b5b68166eaaf7c26b95b2bab27d19450
5865353 <865865353@qq.com>
2019-04-03 00:13:41 +0800

    聊天记录保存

--- web/js/main.js
+++ web/js/main.js
@@ -1,24 +1,25 @@
-function FormatTime(t,date){
+function FormatTime(t, date) {
     var o = {
-        "M+" : date.getMonth()+1,                 //月份
-        "d+" : date.getDate(),                    //日
-        "h+" : date.getHours(),                   //小时
-        "m+" : date.getMinutes(),                 //分
-        "s+" : date.getSeconds(),                 //秒
-        "q+" : Math.floor((date.getMonth()+3)/3), //季度
-        "S"  : date.getMilliseconds()             //毫秒
+        "M+": date.getMonth() + 1,                 //月份
+        "d+": date.getDate(),                    //日
+        "h+": date.getHours(),                   //小时
+        "m+": date.getMinutes(),                 //分
+        "s+": date.getSeconds(),                 //秒
+        "q+": Math.floor((date.getMonth() + 3) / 3), //季度
+        "S": date.getMilliseconds()             //毫秒
     };
-    if(/(y+)/.test(t)){
-        t=t.replace(RegExp.$1,(date.getFullYear()+"").substr(4-RegExp.$1.length));
-    };
-    for(var k in o){
-        if(new RegExp("("+ k +")").test(t)){
-            t=t.replace(RegExp.$1,(RegExp.$1.length==1)?(o[k]):(("00"+ o[k]).substr((""+o[k]).length)));
-        };
+    if (/(y+)/.test(t)) {
+        t = t.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    ;
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(t)) {
+            t = t.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+        ;
     }
     return t;
 };
-
 
 
 let left_arrow = new Vue({
@@ -32,7 +33,7 @@ let left_arrow = new Vue({
                 tx: "img/kk.jpg",
                 name: "测试",
                 msg: "test",
-                infoNum:5
+                infoNum: 5
             }
         ],
     },
@@ -73,19 +74,19 @@ let left_arrow = new Vue({
         },
         //判断好友是否在列表中
         isExitFriend(msg) {
-            msg.infoNum=1;
+            msg.infoNum = 1;
             let result = this.findFriendByid(msg.sid);
             //存在好友列表 //修改信息 必须这样设置 不然不会出发视图更新
             if (result != undefined) {
-                let num=result.value.infoNum;
-                msg.infoNum=num++;
+                let num = result.value.infoNum;
+                msg.infoNum = num++;
                 this.updateValueByIndex(result, msg);
                 //判断是否跟当前好友在聊天
-                    if(msg.sid === right_arrow.sid){
-                        msg.infoNum=0;
-                        right_arrow.updateFriendChatRecord(msg);
-                    }
-                    return;
+                if (msg.sid === right_arrow.sid) {
+                    msg.infoNum = 0;
+                    right_arrow.updateFriendChatRecord(msg);
+                }
+                return;
             }
             // 不存在添加到好友列表中
             this.friendli.unshift(msg);
@@ -108,11 +109,11 @@ let left_arrow = new Vue({
         chatWindow(index, item) {
             //置顶
             this.topByIndex(index);
-            item.infoNum=0;
+            item.infoNum = 0;
             this.$set(this.friendli, 0, item);
             //传值到 右边视图
-            right_arrow.inputValue="";
-            right_arrow.isLt=true;
+            right_arrow.inputValue = "";
+            right_arrow.isLt = true;
             right_arrow.updateWindowInfo(item);
         },
         //将某一列置顶
@@ -123,23 +124,23 @@ let left_arrow = new Vue({
         },
         //修改好友列表某个索引的值
         updateValueByIndex(result, msg) {
-            Object.assign(result.value,msg);
+            Object.assign(result.value, msg);
             left_arrow.topByIndex(result.index);//置顶
             this.$set(this.friendli, 0, result.value);
         },
         //修改好友列表某个索引值的某个字段
-        updateWhereParambByIndex(sid,field,value){
-           let result= this.findFriendByid(sid);
-           if(!result) return;
+        updateWhereParambByIndex(sid, field, value) {
+            let result = this.findFriendByid(sid);
+            if (!result) return;
             this.friendli[result.index][field] = value;
             Vue.set(this.friendli, result.index, this.friendli[result.index]);
         },
         //修改好友列表某个索引值的某 "些" 字段
-        updateWhereParamsbByIndex(sid,fieldObj){
-            let result= this.findFriendByid(sid);
-            if(!result) return;
-            let index=result.index;
-            for(let i in fieldObj){
+        updateWhereParamsbByIndex(sid, fieldObj) {
+            let result = this.findFriendByid(sid);
+            if (!result) return;
+            let index = result.index;
+            for (let i in fieldObj) {
                 this.friendli[index][i] = fieldObj[i];
             }
             Vue.set(this.friendli, index, this.friendli[index]);
@@ -156,13 +157,13 @@ let right_arrow = new Vue({
         tx: "",
         inputValue: "",
         chatRecord: [],
-        isLt:false,
-        isDisab:false
+        isLt: false,
+        isDisab: false
     },
     methods: {
         //展开聊天窗口
         updateWindowInfo(item) {
-            this.chatRecord=[];
+            this.chatRecord = [];
             this.name = item.name;
             this.tx = item.tx;
             this.sid = item.sid;
@@ -172,37 +173,44 @@ let right_arrow = new Vue({
             info = info.split("&&");
             var infoArray = [];
             for (let i in info) {
-                let code = info[i].indexOf("he") !== -1 ? "he" : "me";
-                let msg = info[i].split(":")[1];
-                let tx = code === "he" ? this.tx : left_arrow.tx;
-                infoArray.push({tx, code, msg});
+                let msg = JSON.parse(info[i]);
+                msg.tx = msg.code == "he" ? this.tx : left_arrow.tx;
+                infoArray.push(msg);
             }
             this.chatRecord = infoArray;
+            this.chatContriner();
         },
         //保存消息
-        saveMsg(sid, code, value) {
+        saveMsg(msg, sid) {
+            msg = JSON.parse(msg);
+            sid = sid || msg.sid;
+            msg.code = msg.code == "me" ? msg.code : "he";
+            msg.tx = "";
             //保存消息到本地
             let seinfo = window.localStorage.getItem(sid); //获取聊天记录
+            msg = JSON.stringify(msg);
             //判断是否是第一次发送（接受）消息
-            value = seinfo == null ? code + ":" + value : seinfo + "&&" + code + ":" + value;
-            window.localStorage.setItem(sid, value); //保存  key value
+            msg = seinfo == null ? msg : seinfo + "&&" + msg;
+            window.localStorage.setItem(sid, msg); //保存  key value
         },
         //发送消息
         sendMsg() {
-            if(!this.inputValue)return;
+            if (!this.inputValue) return;
             let info = {
                 tx: left_arrow.tx,
                 code: "me",
                 msg: this.inputValue,
-                date:FormatTime("hh:mm",new Date())
+                date: FormatTime("hh:mm", new Date())
             };
             //发送消息
             ws.send(this.sid + "-" + info.msg);
             //修改好友的最后一条消息记录
-            left_arrow.updateWhereParambByIndex(this.sid,"msg",info.msg);
-            //保存消息到本地
-            this.saveMsg(this.sid, info.code, info.msg);
+            left_arrow.updateWhereParambByIndex(this.sid, "msg", info.msg);
+            //添加到聊天窗口
             this.chatRecord.push(info);
+            this.chatContriner();
+            //保存消息到本地
+            this.saveMsg(JSON.stringify(info), this.sid);
             //清空输入框信息
             this.inputValue = "";
             //
@@ -211,54 +219,76 @@ let right_arrow = new Vue({
         //收到好友发来的消息
         inmessage(msg) {
             //保存消息
-            this.saveMsg(msg.sid, "he", msg.msg);
+            this.saveMsg(JSON.stringify(msg), msg.sid);
             //将好友位置置顶 并修改信息
             let result = left_arrow.findFriendByid(msg.sid);
             if (result !== undefined) {
-                msg.tx=result.value.tx;
+                msg.tx = result.value.tx;
                 left_arrow.updateValueByIndex(result, msg);//修改信息
             }
             //正在跟当前好友聊天
             if (msg.sid === this.sid) {
                 this.updateFriendChatRecord(msg);
-                msg.code="he";
+                msg.code = "he";
                 this.chatRecord.push(msg);
-            }else{
-             //好友未读消息加一
+            } else {
+                //好友未读消息加一
                 let result = left_arrow.findFriendByid(msg.sid);
-                let num=result.value.infoNum;
+                let num = result.value.infoNum;
                 num++;
-                left_arrow.updateWhereParambByIndex(msg.sid,"infoNum",num);
+                left_arrow.updateWhereParambByIndex(msg.sid, "infoNum", num);
             }
 
         },
         //修改聊天记录容器里的信息
-        updateChatRecord(field,value){
-            for (let i in this.chatRecord){
-                if(this.chatRecord[i].code=="he"){
+        updateChatRecord(field, value) {
+            for (let i in this.chatRecord) {
+                if (this.chatRecord[i].code === "he") {
                     this.chatRecord[i][field] = value;
                     Vue.set(this.chatRecord, i, this.chatRecord[i]);
                 }
             }
         },
         //修改容器里好友信息
-        updateFriendChatRecord(msg){
+        updateFriendChatRecord(msg) {
             //填充消息到容器
             this.name = msg.name;
             this.tx = msg.tx;
-            this.updateChatRecord("tx",this.tx);
+            this.updateChatRecord("tx", this.tx);
+        },
+        //关闭聊天窗口
+        closeChat_Window() {
+            //返回上一页
+            window.history.back();
+            this.sid = "";
+            this.name = "";
+            this.tx = "";
+            this.inputValue = "";
+            this.chatRecord = [];
+            this.isLt = false;
+            this.isDisab = false
+        },
+        chatContriner(){
+            let dom=document.querySelector(".chat_content");
+            let scrollHeight = dom.scrollHeight;
+            console.log(scrollHeight)
+            if(scrollHeight>340)return dom.scrollTop=9999999;
+        },
+        //好友下线
+        inerror() {
+            alert("好友已下线");
         }
 
     },
     watch: {
         //监听值
         inputValue(val) {
-            if(!val){
-                this.isDisab=false;
-                document.querySelector("#sendbtn").disabled=true;
-            }else{
-                this.isDisab=true;
-                document.querySelector("#sendbtn").disabled=false;
+            if (!val) {
+                this.isDisab = false;
+                document.querySelector("#sendbtn").disabled = true;
+            } else {
+                this.isDisab = true;
+                document.querySelector("#sendbtn").disabled = false;
             }
         }
     }
@@ -281,11 +311,7 @@ ws.onopen = function () {
 ws.onmessage = function (message) {
     heartCheck.reset();//检测心跳
     let data = message.data;
-    if (data === "&") {
-        this.config.debug && this.log("心跳");
-        return;
-        //遍历消息
-    }
+    if (data === "&") return this.config.debug && this.log("心跳");
     let json_msg = JSON.parse(data);
     console.log(json_msg);
     if (json_msg instanceof Array) return left_arrow.inconnection(json_msg); //好友上线
@@ -293,3 +319,18 @@ ws.onmessage = function (message) {
     right_arrow[code](json_msg); //调用方法
     // code --connection==好友上线	// --inerror ==好友下线 --inmessage 好友消息
 };
+$(function () {
+    // 美化滚动条插件
+    $("body,.chat_content,textarea").niceScroll({
+        cursorcolor: "#808080",
+        cursorborder: "0px none", // CSS方式定义滚动条边框
+        cursorwidth: "4px", // 滚动条的宽度，单位：便素
+    });
+
+});
+
+window.onload = function () {
+    window.addEventListener("popstate", function (e) {
+
+    });
+}
